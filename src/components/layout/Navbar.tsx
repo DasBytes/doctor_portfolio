a99dd224a02442a73936d0df122f282246655422
PD670 <prantadas85463@gmail.com>
2026-01-08 22:09:01 +0600

    changes

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -44,7 +44,7 @@ const Navbar = () => {
             <img
               src="/logo.png"
               alt="Dr. Rakhee Das Dental Hub"
-              className="w-12 h-12 object-cover"
+              className="w-40 h-50 object-cover"
             />
           </Link>
 
